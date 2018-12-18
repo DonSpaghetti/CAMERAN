ba1d8b350147ce6041a11e46afefc4a4ba79b331
Anthony Mignini <37753784+DonSpaghetti@users.noreply.github.com>
2018-12-18 18:27:59 -0500

    Added code for confus command

--- components/botbrain.py
+++ components/botbrain.py
@@ -44,6 +44,19 @@ class BotBrain():
             result = link.get('href')
         return ("http:" + result)
     
+    
+    #for the $confus command
+    async def anconfus():
+        ion = [
+        'https://i.kym-cdn.com/photos/images/original/000/012/974/cat_im_confus20110724-22047-q16ber.jpg',
+        'https://media.giphy.com/media/l3q2K5jinAlChoCLS/200w.gif',
+        'https://media1.tenor.com/images/5b034e96d84c6c6b57a9a04ca14aac02/tenor.gif',
+        'https://media1.tenor.com/images/4cb306a83b1b41da055f47a8071a1934/tenor.gif',
+        ]
+        reply = (random.choice(ion))
+        return reply
+    
+    
     async def rcat(url):
         url = json.loads(requests.get(url='http://aws.random.cat/meow').text)
         cat = url['file']
